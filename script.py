d743fed86266800642aa0bd3fe52b1be00ea6803
ds4geo <67979044+ds4geo@users.noreply.github.com>
2020-12-06 13:26:29 +0100

    Create base script.py

---
+++ script.py
@@ -0,0 +1,7 @@
+# Simple script to demonstrate git branches, pull requests and merging
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+plt.scatter(np.random.rand(50), np.random.rand(50))
