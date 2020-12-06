be8f169b9db7a7ced336f9a204e33b2c4cdc05f5
ds4geo <67979044+ds4geo@users.noreply.github.com>
2020-12-06 13:38:08 +0100

    Add cos and comments

--- script.py
+++ script.py
@@ -4,10 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-
+# random scatter plot
 plt.scatter(np.random.rand(50), np.random.rand(50), color="g")
 
-
-
+# Sine wave
 plt.plot(np.sin(np.linspace(0,math.pi*2,100))
 
+# Cos wave   
+plt.plot(np.cos(np.linspace(0,math.pi*2,100))
