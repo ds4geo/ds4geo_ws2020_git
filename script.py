a89a8f49983573b921710d4e1965c72531118edc
ds4geo <67979044+ds4geo@users.noreply.github.com>
2020-12-09 16:28:35 +0100

    Sine wave colour -> mauve

--- script.py
+++ script.py
@@ -5,5 +5,5 @@ import matplotlib.pyplot as plt
 
 plt.scatter(np.random.rand(50), np.random.rand(50))
 
-plt.plot(np.sin(np.linspace(0,math.pi*2,100))
+plt.plot(np.sin(np.linspace(0,math.pi*2,100), color="xkcd:mauve")
 
