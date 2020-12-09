a990c8c3ded8432802fb0084580b2d541a884a4f
patrickoswald <72376762+patrickoswald@users.noreply.github.com>
2020-12-09 16:45:19 +0100

    Revert "Sine wave colour -> mauve"

--- script.py
+++ script.py
@@ -5,10 +5,6 @@ import matplotlib.pyplot as plt
 
 plt.scatter(np.random.rand(50), np.random.rand(50))
 
-
-plt.plot(np.sin(np.linspace(0,math.pi*2,100), color="xkcd:mauve")
-
-
+plt.plot(np.sin(np.linspace(0,math.pi*5,200))
 print("Hello Python Lovers")
 print("Jana was here")
-
