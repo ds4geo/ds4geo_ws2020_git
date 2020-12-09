787c059a1f81d9ea8dd716accba4732224da06cc
ThoKl <klotz.thomas@gmx.at>
2020-12-09 16:31:34 +0100

    add sns plot

--- script.py
+++ script.py
@@ -2,8 +2,10 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 plt.scatter(np.random.rand(50), np.random.rand(50))
 
 plt.plot(np.sin(np.linspace(0,math.pi*2,100))
 
+sns.scatterplot(x=np.random.rand(50), y=np.random.rand(50), marker=’D’)
