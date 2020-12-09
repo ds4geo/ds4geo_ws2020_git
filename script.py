2c49bacf0d4afd58ba09f2c06a7c5b55b1404bf3
patrickoswald <72376762+patrickoswald@users.noreply.github.com>
2020-12-09 16:27:54 +0100

    Update script.py
    changed some numbers in line 8 and added line9

--- script.py
+++ script.py
@@ -6,4 +6,4 @@ import matplotlib.pyplot as plt
 plt.scatter(np.random.rand(50), np.random.rand(50))
 
 plt.plot(np.sin(np.linspace(0,math.pi*5,200))
-print("Hello World")
+print("Hello Python Lovers")
